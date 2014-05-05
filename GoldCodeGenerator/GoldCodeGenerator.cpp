0bab1c55184e64f3673fee542cc2de5427b1391a
tobiaskarow <mail@tobiaskarow.de>
2014-05-05 19:15:18 +0200

    Generating Chips for the first twelve Satellites

--- GoldCodeGenerator/GoldCodeGenerator.cpp
+++ GoldCodeGenerator/GoldCodeGenerator.cpp
@@ -73,6 +73,7 @@ int GoldCodeGenerator::generateChipSequence(int lengthOfSequence, int tapOne, in
 		lower = shiftLowerRegister(lowerRegister, LENGTH, tapOne, tapTwo);
 		//cout << upper << " XOR " << lower << "  "<< (upper ^ lower) << endl;
 		std::cout << (upper ^ lower);
+		std::cout << ',';
 	}
 	std::cout << std::endl;
 	return 42;
@@ -84,11 +85,22 @@ GoldCodeGenerator::~GoldCodeGenerator()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int length = 1023;
 	GoldCodeGenerator gcg;
-	gcg.generateChipSequence(1024, 2, 6);
-	gcg.generateChipSequence(1024, 3, 7);
 
-
+	gcg.generateChipSequence(length, 2, 6);//1
+	gcg.generateChipSequence(length, 3, 7);//2
+	gcg.generateChipSequence(length, 4, 8);//3
+	gcg.generateChipSequence(length, 5, 9);//4
+	gcg.generateChipSequence(length, 1, 9);//5
+	gcg.generateChipSequence(length, 2, 10);//6
+	gcg.generateChipSequence(length, 1, 8);//7
+	gcg.generateChipSequence(length, 2, 9);//8
+	gcg.generateChipSequence(length, 3, 10); //9
+	gcg.generateChipSequence(length, 2, 3);//10
+	gcg.generateChipSequence(length, 3, 4);//11
+	gcg.generateChipSequence(length, 5, 6);//12
+	
 
 	return 0;
 }
